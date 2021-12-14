04cc5b01800021c2ed5e7c203dfe69c32b6973e2
nmj <3389642992@qq.com>
2021-12-14 19:43:32 +0800

    删除了组件中的导航栏

--- xzs-mysql-master/source/vue/xzs-admin/src/layout/components/TagsView/index.vue
+++ xzs-mysql-master/source/vue/xzs-admin/src/layout/components/TagsView/index.vue
@@ -1,21 +1,5 @@
 <template>
   <div id="tags-view-container" class="tags-view-container">
-    <scroll-pane ref="scrollPane" class="tags-view-wrapper">
-      <router-link
-        v-for="tag in visitedViews"
-        ref="tag"
-        :key="tag.path"
-        :class="isActive(tag)?'active':''"
-        :to="{ path: tag.path, query: tag.query, fullPath: tag.fullPath }"
-        tag="span"
-        class="tags-view-item"
-        @click.middle.native="closeSelectedTag(tag)"
-        @contextmenu.prevent.native="openMenu(tag,$event)"
-      >
-        {{ tag.title }}
-        <span v-if="!tag.meta.affix" class="el-icon-close" @click.prevent.stop="closeSelectedTag(tag)" />
-      </router-link>
-    </scroll-pane>
     <ul v-show="visible" :style="{left:left+'px',top:top+'px'}" class="contextmenu">
       <li @click="refreshSelectedTag(selectedTag)">刷新</li>
       <li v-if="!(selectedTag.meta&&selectedTag.meta.affix)" @click="closeSelectedTag(selectedTag)">关闭</li>
@@ -31,7 +15,7 @@ import path from 'path'
 import { mapGetters } from 'vuex'
 
 export default {
-  components: { ScrollPane },
+  components: {},
   data () {
     return {
       visible: false,

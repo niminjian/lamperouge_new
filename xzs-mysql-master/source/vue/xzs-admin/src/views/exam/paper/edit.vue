e39acbee68ffc4a6d03bb9811e74428607821707
shaoyanting <1478244871@qq.com>
2022-04-06 09:25:26 +0800

    添加模板试卷

--- xzs-mysql-master/source/vue/xzs-admin/src/views/exam/paper/edit.vue
+++ xzs-mysql-master/source/vue/xzs-admin/src/views/exam/paper/edit.vue
@@ -114,7 +114,7 @@
         <el-button type="primary" @click="submitForm">提交</el-button>
         <el-button @click="resetForm">重置</el-button>
         <el-button type="success" @click="addTitle">添加标题</el-button>
-        <el-button type="success" plain @click="addPaper"
+        <el-button type="success" plain @click="addPaper(titleItem)"
           >快速组卷</el-button
         >
       </el-form-item>
@@ -122,21 +122,25 @@
     <!-- 快速组卷弹窗 -->
     <el-dialog :visible.sync="paperPage.showDialog" width="70%">
       <el-form :model="paperPage.queryParam" ref="queryForm" :inline="true">
-        <el-form-item label="测试ID：">
+        <el-form-item label="测试1ID：">
           <el-input v-model="paperPage.queryParam.id" clearable></el-input>
         </el-form-item>
         <el-form-item label="题型：">
-          <el-select v-model="paperPage.queryParam.questionType" clearable>
+          <el-select
+            v-model="paperPage.queryParam.questionType"
+            clearable
+            @change="styleChange"
+          >
             <el-option
-              v-for="item in questionTypeEnum"
+              v-for="item in pageClass"
               :key="item.key"
-              :value="item.key"
-              :label="item.value"
+              :value="item.value"
+              :label="item.label"
             ></el-option>
           </el-select>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="queryForm">查询</el-button>
+          <el-button type="primary" @click="queryForm2">查询</el-button>
         </el-form-item>
       </el-form>
       <el-table
@@ -167,7 +171,7 @@
       />
       <span slot="footer" class="dialog-footer">
         <el-button @click="paperPage.showDialog = false">取 消</el-button>
-        <el-button type="primary" @click="confirmQuestionSelect"
+        <el-button type="primary" @click="confirmQuestionSelect2"
           >确定</el-button
         >
       </span>
@@ -226,6 +230,131 @@
         >
       </span>
     </el-dialog>
+
+    <el-card class="box-card" v-show="currentShowList[0]">
+      <div slot="header" class="clearfix">
+        <span class="title">选择题训练</span>
+        <el-button
+          style="float: right; padding: 3px 0"
+          type="text"
+          @click="currentShowList[0] = false"
+          >删除</el-button
+        >
+      </div>
+      <div>
+        <div class="title item">
+          第1题<el-button style="float: right; padding: 3px 0" type="text"
+            >替换</el-button
+          >
+        </div>
+        <div class="question">1+2=?</div>
+        <div class="itemList">
+          <p>A. 1</p>
+          <p>B. 2</p>
+          <p>C. 3</p>
+          <p>D. 4</p>
+        </div>
+      </div>
+      <div>
+        <div class="title item">
+          第2题<el-button style="float: right; padding: 3px 0" type="text"
+            >替换</el-button
+          >
+        </div>
+        <div class="question">π=?</div>
+        <div class="itemList">
+          <p>A. 3.15</p>
+          <p>B. 3.14159</p>
+          <p>C. 3</p>
+          <p>D. 3.1416</p>
+        </div>
+      </div>
+      <div>
+        <div class="title item">
+          第3题<el-button style="float: right; padding: 3px 0" type="text"
+            >替换</el-button
+          >
+        </div>
+        <div class="question">23*32=?</div>
+        <div class="itemList">
+          <p>A. 736</p>
+          <p>B. 763</p>
+          <p>C. 756</p>
+          <p>D. 716</p>
+        </div>
+      </div>
+    </el-card>
+    <el-card class="box-card" v-show="currentShowList[1]">
+      <div slot="header" class="clearfix">
+        <span class="title">填空题训练</span>
+        <el-button style="float: right; padding: 3px 0" type="text"
+          >删除</el-button
+        >
+      </div>
+      <div>
+        <div class="title item">
+          第1题<el-button style="float: right; padding: 3px 0" type="text"
+            >替换</el-button
+          >
+        </div>
+        <div class="question">衬衫的价格是___便士</div>
+      </div>
+      <div>
+        <div class="title item">
+          第2题<el-button style="float: right; padding: 3px 0" type="text"
+            >替换</el-button
+          >
+        </div>
+        <div class="question">万有引力公式___</div>
+      </div>
+      <div>
+        <div class="title item">
+          第3题<el-button style="float: right; padding: 3px 0" type="text"
+            >替换</el-button
+          >
+        </div>
+        <div class="question">奇变偶不变，___</div>
+      </div>
+    </el-card>
+
+    <el-card class="box-card" v-show="currentShowList[2]">
+      <div slot="header" class="clearfix">
+        <span class="title">综合题训练</span>
+        <el-button style="float: right; padding: 3px 0" type="text"
+          >删除</el-button
+        >
+      </div>
+      <div>
+        <div class="title item">
+          第1题<el-button style="float: right; padding: 3px 0" type="text"
+            >替换</el-button
+          >
+        </div>
+        <div class="question">
+          东风洗染厂，每天用水量比过去节约20%，原有390吨水，现在比过去多用30天，现在每天用水多少吨?
+        </div>
+      </div>
+      <div>
+        <div class="title item">
+          第2题<el-button style="float: right; padding: 3px 0" type="text"
+            >替换</el-button
+          >
+        </div>
+        <div class="question">
+          一辆汽车从甲地开往乙地，每小时行驶28千米，4.5小时到达，要4小时到达，每小时要多行几千米?
+        </div>
+      </div>
+      <div>
+        <div class="title item">
+          第3题<el-button style="float: right; padding: 3px 0" type="text"
+            >替换</el-button
+          >
+        </div>
+        <div class="question">
+          一块平行四边形菜地，底与高的和是150米，它们的比是3∶2，求这块菜地的面积是多少平方米?
+        </div>
+      </div>
+    </el-card>
   </div>
 </template>
 
@@ -280,6 +409,7 @@ export default {
         total: 0,
       },
       currentTitleItem: null,
+      currentShowList: [false, false, false],
       paperPage: {
         multipleSelection: [],
         showDialog: false,
@@ -294,6 +424,24 @@ export default {
         tableData: [],
         total: 0,
       },
+      pageClass: [
+        {
+          value: "0",
+          label: "选择题训练",
+        },
+        {
+          value: "1",
+          label: "填空题训练",
+        },
+        {
+          value: "2",
+          label: "综合题训练",
+        },
+        {
+          value: "3",
+          label: "附加题训练",
+        },
+      ],
     };
   },
   created() {
@@ -311,8 +459,17 @@ export default {
     }
   },
   methods: {
-    addPaper() {
-      // this.currentTitleItem = titleItem;
+    styleChange(val) {
+      if (val === "0") {
+        this.currentShowList[0] = true;
+      } else if (val === "1") {
+        this.currentShowList[1] = true;
+      } else if (val === "2") {
+        this.currentShowList[2] = true;
+      }
+    },
+    addPaper(titleItem) {
+      this.currentTitleItem = titleItem;
       this.paperPage.showDialog = true;
       this.search2();
     },
@@ -374,6 +531,10 @@ export default {
       this.questionPage.queryParam.pageIndex = 1;
       this.search();
     },
+    queryForm2() {
+      this.questionPage.queryParam.pageIndex = 1;
+      this.search2();
+    },
     confirmQuestionSelect() {
       let _this = this;
       this.questionPage.multipleSelection.forEach((q) => {
@@ -382,6 +543,15 @@ export default {
         });
       });
       this.questionPage.showDialog = false;
+    },
+    confirmQuestionSelect2() {
+      let _this = this;
+      this.paperPage.multipleSelection.forEach((q) => {
+        questionApi.select(q.id).then((re) => {
+          _this.currentTitleItem.questionItems.push(re.response);
+        });
+      });
+      this.paperPage.showDialog = false;
     },
     levelChange() {
       this.form.subjectId = null;
@@ -439,12 +609,52 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style scoped lang="scss" >
 .exampaper-item-box {
   .q-title {
     margin: 0px 5px 0px 5px;
   }
   .q-item-content {
   }
+}
+.text {
+  font-size: 15px;
+}
+.title {
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.item {
+  margin-bottom: 13px;
+  padding-left: 20px;
+}
+.itemList {
+  width: 80%;
+  margin-left: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.question {
+  margin-left: 40px;
+  font-size: 16px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+
+.box-card {
+  width: 93%;
+  margin-left: 90px;
+  margin-top: 30px;
 }
 </style>
